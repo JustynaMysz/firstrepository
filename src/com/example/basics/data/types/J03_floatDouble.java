a090bc9c0c874cf3b4b479ddd489a4ade47f913b
justyna.myszkiewicz85@gmail.com <justyna.myszkiewicz85@gmail.com>
2021-09-19 12:11:16 +0200

    drugi commit

---
+++ src/com/example/basics/data/types/J03_floatDouble.java
@@ -0,0 +1,12 @@
+package com.example.basics.data.types;
+
+public class J03_floatDouble {
+    public static void main(String[] args) {
+        float number = 10.5f;
+        double bigNumber = 23.65678648656765;
+
+        System.out.println("number"+ number
+                         + "bigNumber" + bigNumber);
+    }
+}
+
---
+++ src/com/example/basics/data/types/J05_variableNames.java
@@ -0,0 +1,12 @@
+package com.example.basics.data.types;
+
+public class J05_variableNames {
+    public static void main(String[] args) {
+        int _int1 = 10;
+        int currentHour = 10;
+        float moneyInThePocket = 49.99f;
+        float MoneyInThePocket = 39.99f;
+        double $speed = 100.10d;
+
+    }
+}
